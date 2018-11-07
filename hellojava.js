d42585915bb3195c160f562076ba932d5985dd2d
jbest2021 <43757518+jbest2021@users.noreply.github.com>
2018-11-06 22:37:10 -0500

    done

--- hellojava.js
+++ hellojava.js
@@ -1,3 +1,19 @@
 function sayHello () {
   console.log("Hello, World!");
 }
+
+function sayHi () {
+  alert("Hello, World!");
+}
+
+function sayHey () {
+  var p=document.getElementById("sayHey");
+  p.innerHTML = "Hello, World!";
+}
+
+function sayHola () {
+  var username = prompt("Enter Your Username"); 
+  var greeting = document.getElementById("sayHola");
+  greeting.innerHTML = "Hello, " + username + "!";
+}
+  
